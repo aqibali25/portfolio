9227f6a5d4773c6c7acd16c75287706d4aad9535
aqibali25 <aqibalikalwar1@gmail.com>
2024-09-01 00:24:10 +0500

    Create resume files and Make layout responsive

--- assets/Js/script.js
+++ assets/Js/script.js
@@ -1,6 +1,4 @@
 document.addEventListener("DOMContentLoaded", function () {
-  console.log("DOM fully loaded and parsed");
-
   const savedTheme = localStorage.getItem("theme");
 
   const themeIcon = document.getElementById("theme-icon");
@@ -8,13 +6,7 @@ document.addEventListener("DOMContentLoaded", function () {
   const logo = document.querySelector(".logo img");
   const body = document.body;
 
-  console.log("Theme Icon:", themeIcon);
-  console.log("Nav Links:", navLinks);
-  console.log("Logo:", logo);
-  console.log("Body:", body);
-
   if (savedTheme) {
-    console.log("Saved Theme:", savedTheme);
     if (savedTheme === "dark") {
       applyDarkTheme();
     } else {
@@ -24,7 +16,6 @@ document.addEventListener("DOMContentLoaded", function () {
     const isSystemDark = window.matchMedia(
       "(prefers-color-scheme: dark)"
     ).matches;
-    console.log("System Dark Mode:", isSystemDark);
     if (isSystemDark) {
       applyDarkTheme();
     } else {
@@ -37,7 +28,6 @@ document.addEventListener("DOMContentLoaded", function () {
     themeIcon.setAttribute("src", "assets/images/light-mode.png");
     logo.setAttribute("src", "assets/images/logo-white.png");
     localStorage.setItem("theme", "dark");
-    console.log("Applied Dark Theme");
   }
 
   function applyLightTheme() {
@@ -45,11 +35,9 @@ document.addEventListener("DOMContentLoaded", function () {
     themeIcon.setAttribute("src", "assets/images/dark-mode.png");
     logo.setAttribute("src", "assets/images/logo-black.png");
     localStorage.setItem("theme", "light");
-    console.log("Applied Light Theme");
   }
 
   themeIcon.addEventListener("click", function () {
-    console.log("Theme Icon Clicked");
     if (body.classList.contains("dark-theme")) {
       applyLightTheme();
     } else {
@@ -58,31 +46,29 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 
   const toggleMenu = document.getElementById("menu-toggle");
-  console.log("Toggle Menu:", toggleMenu);
-  if (toggleMenu) {
-    toggleMenu.addEventListener("click", function () {
-      const navbar = document.querySelector(".navbar");
-      const mainContent = document.querySelector(".content");
+  toggleMenu.addEventListener("click", function () {
+    const navbar = document.querySelector(".navbar");
+    const mainContent = document.querySelector(".content");
 
-      if (navbar && mainContent) {
-        navbar.classList.toggle("active");
-        this.classList.toggle("fa-x");
-        if (toggleMenu.classList.contains("fa-x")) {
-          toggleMenu.style.fontSize = "20px";
-        } else {
-          toggleMenu.style.fontSize = "35px";
-        }
-        if (navbar.classList.contains("active")) {
-          mainContent.style.marginTop = navLinks.offsetHeight + 5 + "px";
-        } else {
-          mainContent.style.marginTop = "0px";
-        }
-        console.log("Menu toggled");
+    if (navbar && mainContent) {
+      navbar.classList.toggle("active");
+      this.classList.toggle("fa-x");
+      if (toggleMenu.classList.contains("fa-x")) {
+        toggleMenu.style.fontSize = "20px";
       } else {
-        console.error("Navbar or Main Content not found");
+        toggleMenu.style.fontSize = "35px";
       }
-    });
-  } else {
-    console.error("Toggle Menu button not found");
-  }
+      if (navbar.classList.contains("active")) {
+        mainContent.style.marginTop = navLinks.offsetHeight + 5 + "px";
+      } else {
+        mainContent.style.marginTop = "0px";
+      }
+    }
+  });
+});
+document.addEventListener("DOMContentLoaded", function () {
+  AOS.init({
+    duration: 1500,
+    once: true,
+  });
 });
